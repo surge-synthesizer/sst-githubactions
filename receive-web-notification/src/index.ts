b9d459872905dac18994fe3b98b93f20eb1ec3fb
Mike Thierman <mthierman@gmail.com>
2025-10-29 08:19:24 -0600

    Feature: surge-synth-team.org-notify & surge-synth-team.org-receive (#6)

---
+++ receive-web-notification/src/index.ts
@@ -0,0 +1,26 @@
+import { execSync } from "node:child_process";
+import { readFile, writeFile } from "node:fs/promises";
+import { join, resolve } from "node:path";
+
+(async () => {
+    const event_path = process.env.GITHUB_EVENT_PATH;
+
+    if (!event_path) {
+        throw new Error("GITHUB_EVENT_PATH not set");
+    }
+
+    const event = JSON.parse(await readFile(event_path, "utf8"));
+
+    await writeFile(
+        join(resolve("src/content/generated"), `${event.event_type}.json`),
+        JSON.stringify(event.client_payload.data, null, 4),
+    );
+
+    execSync(
+        `git config user.name github-actions[bot] &&
+    git config user.email 41898282+github-actions[bot]@users.noreply.github.com &&
+    git add -A &&
+    (git commit -m 'github-actions: Update generated files' || echo "No changes to commit") &&
+    git push origin main`,
+    );
+})();
---
+++ send-web-notification/src/index.ts
@@ -0,0 +1,63 @@
+import { getInput } from "@actions/core";
+import { Temporal } from "@js-temporal/polyfill";
+import { Octokit } from "@octokit/rest";
+import { execSync, spawnSync } from "node:child_process";
+
+(async () => {
+    const web_repo = getInput("web_repo", { required: true });
+    const event_type = getInput("event_type", { required: true });
+    const name = getInput("name", { required: true });
+    const version = getInput("version", { required: true });
+
+    const releasesInput = getInput("releases", { required: false }) || "{}";
+    let releases: Record<string, string>;
+
+    try {
+        releases = JSON.parse(releasesInput);
+    } catch (error) {
+        throw new Error(`Invalid JSON for releases input: ${error}`);
+    }
+
+    const dataInput = getInput("data", { required: false }) || "{}";
+    let data: Record<string, string>;
+
+    try {
+        data = JSON.parse(dataInput);
+    } catch (error) {
+        throw new Error(`Invalid JSON for data input: ${error}`);
+    }
+
+    const octokit = new Octokit({ auth: process.env.GH_TOKEN });
+
+    await octokit.repos.createDispatchEvent({
+        owner: "surge",
+        repo: web_repo,
+        event_type,
+        client_payload: {
+            data: {
+                name,
+                version,
+                project_repo: execSync(`gh repo view --json url --jq '.url'`),
+                releases,
+                data,
+                build_time: Temporal.Now.plainDateTimeISO().toString(),
+                latest_commit: spawnSync("git", ["rev-parse", "--short", "HEAD"], {
+                    encoding: "utf-8",
+                }).stdout.trim(),
+                recent_commits: spawnSync(
+                    "git",
+                    ["log", "-5", "--pretty=format:%h%x00%an%x00%aI%x00%s%x00"],
+                    {
+                        encoding: "utf-8",
+                    },
+                )
+                    .stdout.split("\n")
+                    .filter(Boolean)
+                    .map((line) => {
+                        const [commit, author, date, message] = line.split("\x00");
+                        return { commit, author, date, message };
+                    }),
+            },
+        },
+    });
+})();
